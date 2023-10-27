bd41516371a77e67249fddf0f4e8e3a3ed797406
Indrek Soidla <indrek.soidla@ut.ee>
2023-10-27 06:17:20 +0300

    Initial commit

---
+++ ka_4_usaldusvahemik_Rs.R
@@ -0,0 +1,4 @@
+# Kvantitatiivsete meetodite aluskursus
+# Praktikumi 4 eel: usaldusvahemik R-s
+
+# Siin saate kaasa teha praktikumieelse juhendmaterjali tegevusi, kopeerides juhendist koodijupid või juhendi põhjal need ise sisestades. Kui tekib probleeme või vajate abi, võtke õppejõuga ühendust.
